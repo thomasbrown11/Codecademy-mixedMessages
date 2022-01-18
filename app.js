936adf6753c35ae76ac7a51e47c88218ba095536
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2022-01-18 14:04:48 -0500

    filled out arrays and tested Math.random functionality

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const luckArray = [];
-const fortuneArray = [];
-const directionArray = [];
+const luckArray = ["good", "bad", "fair", "dangerous", "great"];
+const fortuneArray = ['You will inherit a large sum of money',
+  'You will meet a new friend', 'You will gain noteriety in a relevant field',
+  'You will lose something important', 'You will find the answer soon'];
+const directionArray = ['Avoid a risky situation', 'Reach out to a loved one',
+  'Heed upcoming advice', 'Take a new risk', 'Take some time for yourself'];
 
+//console.log(luckArray[Math.floor(Math.random() * 5)]);
+//console.log(fortuneArray[Math.floor(Math.random() * 5)]);
+//console.log(directionArray[Math.floor(Math.random() * 5)]);
