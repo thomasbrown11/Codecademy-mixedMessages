e915b194870b8b0b3d1654b9570626411ac701fa
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2022-01-18 14:14:12 -0500

    finished basic functionality... awaiting implementation

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const luckArray = ["good", "bad", "fair", "dangerous", "great"];
+let randomLuck = luckArray[Math.floor(Math.random() * 5)];
 const fortuneArray = ['You will inherit a large sum of money',
   'You will meet a new friend', 'You will gain noteriety in a relevant field',
   'You will lose something important', 'You will find the answer soon'];
-const directionArray = ['Avoid a risky situation', 'Reach out to a loved one',
-  'Heed upcoming advice', 'Take a new risk', 'Take some time for yourself'];
+let randomFortune = fortuneArray[Math.floor(Math.random() * 5)];
+const directionArray = ['avoid a risky situation', 'reach out to a loved one',
+  'heed upcoming advice', 'take a new risk', 'take some time for yourself'];
+let randomDirection = directionArray[Math.floor(Math.random() * 5)];
 
 //console.log(luckArray[Math.floor(Math.random() * 5)]);
 //console.log(fortuneArray[Math.floor(Math.random() * 5)]);
 //console.log(directionArray[Math.floor(Math.random() * 5)]);
+console.log(`You are having ${randomLuck} luck. ${randomFortune}. You should 
+${randomDirection}.`); 
